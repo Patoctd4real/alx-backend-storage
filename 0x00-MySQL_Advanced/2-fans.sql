3cc510768d76c6c0cf360a09f3c83b5a9c555ddf
patoctd4real <patoctd4real@yahoo.com>
2023-05-06 22:06:10 +0000

    sql

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,3 @@
+-- by the number of (non-unique) fans
+SELECT origin, SUM(fans) as nb_fans from metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,2 @@
+-- the table names and the first letter of name.
+CREATE INDEX idx_name_first ON names(name(1))
