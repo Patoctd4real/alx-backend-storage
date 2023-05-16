83e231666bc1b2ebcbf21b817f79ae4e909366ac
patoctd4real <patoctd4real@yahoo.com>
2023-05-16 03:42:43 +0000

    0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-"""a function named index_range that takes
-two integer arguments page and page_size.
 """
+Simple helper function
+"""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
-    """tuple containing page and the page_size """
-
-    opening_index = (page - 1) * page_size
-    closing_index = opening_index + page_size
-    return (opening_index, closing_index)
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    return a tuple of size two containing a start
+     those particular pagination parameters
+    :param page:
+    :param page_size:
+    :return:
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
